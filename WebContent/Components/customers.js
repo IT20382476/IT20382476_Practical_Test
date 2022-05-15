2771ced3dfeb6cc67e9ec4dac043a7b179ed9994
varnavi430 <71841456+varnavi430@users.noreply.github.com>
2022-05-15 17:49:17 +0530

    exported database

--- WebContent/Components/customers.js
+++ WebContent/Components/customers.js
@@ -58,7 +58,7 @@ $(document).on("click", ".btnUpdate", function (event) {
   $("#lastName").val($(this).closest("tr").find("td:eq(1)").text());
   $("#nic").val($(this).closest("tr").find("td:eq(2)").text());
   $("#phone").val($(this).closest("tr").find("td:eq(3)").text());
-  $("#email").val($(this).closest("tr").find("td:eq(3)").text());
+  $("#email").val($(this).closest("tr").find("td:eq(4)").text());
 
 
 });
